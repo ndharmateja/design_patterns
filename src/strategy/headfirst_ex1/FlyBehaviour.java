96ba07be0f39804f182d0170ab0a5f1315045e9d
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-10-22 12:33:50 +0530

    Create interfaces for fly and quack behaviours

---
+++ src/strategy/headfirst_ex1/FlyBehaviour.java
@@ -0,0 +1,5 @@
+package strategy.headfirst_ex1;
+
+public interface FlyBehaviour {
+    public void fly();
+}
---
+++ src/strategy/headfirst_ex1/QuackBehaviour.java
@@ -0,0 +1,5 @@
+package strategy.headfirst_ex1;
+
+public interface QuackBehaviour {
+    public void quack();
+}
