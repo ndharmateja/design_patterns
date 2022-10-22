c1914ef62d8ba204b056b27cc49c559b92050718
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-10-22 13:05:01 +0530

    Duck sim

--- src/strategy/headfirst_ex1/DuckSimulator.java
+++ src/strategy/headfirst_ex1/DuckSimulator.java
@@ -2,6 +2,8 @@ package strategy.headfirst_ex1;
 
 import strategy.headfirst_ex1.duck.Duck;
 import strategy.headfirst_ex1.duck.MallardDuck;
+import strategy.headfirst_ex1.duck.ModelDuck;
+import strategy.headfirst_ex1.fly.FlyRocketPowered;
 
 public class DuckSimulator {
     public static void main(String[] args) {
@@ -9,5 +11,12 @@ public class DuckSimulator {
         duck.display();
         duck.performFly();
         duck.performQuack();
+
+        Duck model = new ModelDuck();
+        model.display();
+        model.performFly();
+        model.performQuack();
+        model.setFlyBehaviour(new FlyRocketPowered());
+        model.performFly();
     }
 }
