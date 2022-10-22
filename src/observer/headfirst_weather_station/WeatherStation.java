c2777a1ad9e3d8c0318013fd364fcae4f98cc5ec
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-10-22 17:19:12 +0530

    Remove unused variables

--- src/observer/headfirst_weather_station/WeatherStation.java
+++ src/observer/headfirst_weather_station/WeatherStation.java
@@ -8,11 +8,11 @@ public class WeatherStation {
     public static void main(String[] args) {
         WeatherData weatherData = new WeatherData();
 
-        HeatIndexDisplay heatIndexDisplay = new HeatIndexDisplay(weatherData);
-        StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
+        new HeatIndexDisplay(weatherData);
+        new StatisticsDisplay(weatherData);
         weatherData.setValues(80, 65, 30.4);
 
-        CurrentConditionsDisplay currentConditionsDisplay = new CurrentConditionsDisplay(weatherData);
+        new CurrentConditionsDisplay(weatherData);
         weatherData.setValues(78, 90, 29.2);
         weatherData.setValues(82, 70, 29.2);
     }
