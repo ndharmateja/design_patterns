b567650ff9503ca287dc799d3771494dcf9ee775
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-10-22 14:55:16 +0530

    Add WeatherData class

---
+++ src/observer/headfirst_weather_station/WeatherData.java
@@ -0,0 +1,45 @@
+package observer.headfirst_weather_station;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WeatherData implements Subject {
+
+    List<Observer> observers;
+    double temperature;
+    double humidity;
+    double pressure;
+
+    public WeatherData() {
+        this.observers = new ArrayList<>();
+    }
+
+    @Override
+    public void addObserver(Observer o) {
+        this.observers.add(o);
+    }
+
+    @Override
+    public void removeObserver(Observer o) {
+        this.observers.remove(o);
+    }
+
+    @Override
+    public void notifyListeners() {
+        for (Observer o : observers) {
+            o.update(this.temperature, this.humidity, this.pressure);
+        }
+    }
+
+    public void measurementsChanged() {
+        notifyListeners();
+    }
+
+    public void setValues(double temp, double humidity, double pressure) {
+        this.temperature = temp;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        measurementsChanged();
+    }
+
+}
