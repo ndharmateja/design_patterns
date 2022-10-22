1d0162e7f9e709689c7519872f1975738b3bcb20
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-10-22 12:35:59 +0530

    Implement concrete classes for flying

---
+++ src/strategy/headfirst_ex1/fly/FlyNoWay.java
@@ -0,0 +1,10 @@
+package strategy.headfirst_ex1.fly;
+
+public class FlyNoWay implements FlyBehaviour {
+
+    @Override
+    public void fly() {
+        System.out.println("I cannot fly.");
+    }
+
+}
