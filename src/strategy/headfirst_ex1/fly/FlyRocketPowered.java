a3034ffe408dbd86928b5eef7fa6cf20f3cf0efb
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-10-22 13:04:55 +0530

    Add fly rocket behaviour

---
+++ src/strategy/headfirst_ex1/fly/FlyRocketPowered.java
@@ -0,0 +1,10 @@
+package strategy.headfirst_ex1.fly;
+
+public class FlyRocketPowered implements FlyBehaviour {
+
+    @Override
+    public void fly() {
+        System.out.println("I am flying with a rocket.");
+    }
+
+}
