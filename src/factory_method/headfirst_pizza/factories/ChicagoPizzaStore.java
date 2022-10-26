fa0f3ee2c23494df532b62ced64af8fa4928cd10
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-10-26 12:00:10 +0530

    Implement ChicagoPizzaStore

---
+++ src/factory_method/headfirst_pizza/factories/ChicagoPizzaStore.java
@@ -0,0 +1,24 @@
+package factory_method.headfirst_pizza.factories;
+
+import factory_method.headfirst_pizza.pizzas.Pizza;
+import factory_method.headfirst_pizza.pizzas.chicago.ChicagoStyleCheesePizza;
+import factory_method.headfirst_pizza.pizzas.chicago.ChicagoStyleClamPizza;
+import factory_method.headfirst_pizza.pizzas.chicago.ChicagoStylePepperoniPizza;
+import factory_method.headfirst_pizza.pizzas.chicago.ChicagoStyleVeggiePizza;
+
+public class ChicagoPizzaStore extends PizzaStore {
+
+    @Override
+    public Pizza createPizza(String type) {
+        if (type.equals("cheese"))
+            return new ChicagoStyleCheesePizza();
+        if (type.equals("clam"))
+            return new ChicagoStyleClamPizza();
+        if (type.equals("pepperoni"))
+            return new ChicagoStylePepperoniPizza();
+        if (type.equals("veggie"))
+            return new ChicagoStyleVeggiePizza();
+        return null;
+    }
+
+}
