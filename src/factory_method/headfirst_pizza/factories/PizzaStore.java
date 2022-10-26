bf0d918597933a0403d1c2eec49d4c5e67b5b9f9
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-10-26 11:58:16 +0530

    Add pizza store abstract class

---
+++ src/factory_method/headfirst_pizza/factories/PizzaStore.java
@@ -0,0 +1,19 @@
+package factory_method.headfirst_pizza.factories;
+
+import factory_method.headfirst_pizza.pizzas.Pizza;
+
+public abstract class PizzaStore {
+
+    public Pizza orderPizza(String type) {
+        Pizza pizza = this.createPizza(type);
+
+        pizza.prepare();
+        pizza.bake();
+        pizza.cut();
+        pizza.box();
+
+        return pizza;
+    }
+
+    public abstract Pizza createPizza(String type);
+}
