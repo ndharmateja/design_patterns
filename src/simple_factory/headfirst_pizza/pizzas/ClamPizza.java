a80da0d541e12771c54f67e907f4d05bd4acbce7
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-10-26 11:43:14 +0530

    Add pizza classes

---
+++ src/simple_factory/headfirst_pizza/pizzas/ClamPizza.java
@@ -0,0 +1,29 @@
+package simple_factory.headfirst_pizza.pizzas;
+
+public class ClamPizza implements Pizza {
+
+    @Override
+    public void prepare() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void bake() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void cut() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void box() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ src/simple_factory/headfirst_pizza/pizzas/Pizza.java
@@ -0,0 +1,11 @@
+package simple_factory.headfirst_pizza.pizzas;
+
+public interface Pizza {
+    public void prepare();
+
+    public void bake();
+
+    public void cut();
+
+    public void box();
+}
