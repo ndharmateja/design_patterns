cedadca48d41ce3c18e2c551930f0ee64b78fefd
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-10-26 12:18:52 +0530

    Add driver class

---
+++ src/factory/abstract_factory/headfirst_pizza/PizzaTestDrive.java
@@ -0,0 +1,38 @@
+package factory.abstract_factory.headfirst_pizza;
+
+import factory.abstract_factory.headfirst_pizza.pizzas.Pizza;
+import factory.abstract_factory.headfirst_pizza.stores.ChicagoPizzaStore;
+import factory.abstract_factory.headfirst_pizza.stores.NYPizzaStore;
+import factory.abstract_factory.headfirst_pizza.stores.PizzaStore;
+
+public class PizzaTestDrive {
+
+	public static void main(String[] args) {
+		PizzaStore nyStore = new NYPizzaStore();
+		PizzaStore chicagoStore = new ChicagoPizzaStore();
+
+		Pizza pizza = nyStore.orderPizza("cheese");
+		System.out.println("Ethan ordered a " + pizza + "\n");
+
+		pizza = chicagoStore.orderPizza("cheese");
+		System.out.println("Joel ordered a " + pizza + "\n");
+
+		pizza = nyStore.orderPizza("clam");
+		System.out.println("Ethan ordered a " + pizza + "\n");
+
+		pizza = chicagoStore.orderPizza("clam");
+		System.out.println("Joel ordered a " + pizza + "\n");
+
+		pizza = nyStore.orderPizza("pepperoni");
+		System.out.println("Ethan ordered a " + pizza + "\n");
+
+		pizza = chicagoStore.orderPizza("pepperoni");
+		System.out.println("Joel ordered a " + pizza + "\n");
+
+		pizza = nyStore.orderPizza("veggie");
+		System.out.println("Ethan ordered a " + pizza + "\n");
+
+		pizza = chicagoStore.orderPizza("veggie");
+		System.out.println("Joel ordered a " + pizza + "\n");
+	}
+}
