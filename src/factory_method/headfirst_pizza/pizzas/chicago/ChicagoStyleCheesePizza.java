b406963e9a6d67681f68de9f501fb97fec43ce42
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-10-26 11:54:43 +0530

    Add chicago pizzas

---
+++ src/factory_method/headfirst_pizza/pizzas/chicago/ChicagoStyleCheesePizza.java
@@ -0,0 +1,18 @@
+package factory_method.headfirst_pizza.pizzas.chicago;
+
+import factory_method.headfirst_pizza.pizzas.Pizza;
+
+public class ChicagoStyleCheesePizza extends Pizza {
+
+	public ChicagoStyleCheesePizza() {
+		name = "Chicago Style Deep Dish Cheese Pizza";
+		dough = "Extra Thick Crust Dough";
+		sauce = "Plum Tomato Sauce";
+
+		toppings.add("Shredded Mozzarella Cheese");
+	}
+
+	protected void cut() {
+		System.out.println("Cutting the pizza into square slices");
+	}
+}
