e6e8d69118feca1c3073753e62aa52dc61608a43
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-10-26 11:58:26 +0530

    Implement NyPizzaStore class

---
+++ src/factory_method/headfirst_pizza/factories/NyPizzaStore.java
@@ -0,0 +1,24 @@
+package factory_method.headfirst_pizza.factories;
+
+import factory_method.headfirst_pizza.pizzas.Pizza;
+import factory_method.headfirst_pizza.pizzas.ny.NYStyleCheesePizza;
+import factory_method.headfirst_pizza.pizzas.ny.NYStyleClamPizza;
+import factory_method.headfirst_pizza.pizzas.ny.NYStylePepperoniPizza;
+import factory_method.headfirst_pizza.pizzas.ny.NYStyleVeggiePizza;
+
+public class NyPizzaStore extends PizzaStore {
+
+    @Override
+    public Pizza createPizza(String type) {
+        if (type.equals("cheese"))
+            return new NYStyleCheesePizza();
+        if (type.equals("clam"))
+            return new NYStyleClamPizza();
+        if (type.equals("pepperoni"))
+            return new NYStylePepperoniPizza();
+        if (type.equals("veggie"))
+            return new NYStyleVeggiePizza();
+        return null;
+    }
+
+}
